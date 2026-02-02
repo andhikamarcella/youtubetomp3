53e8664b83575c6e1c9d8aa794a8b9f7258bd10f
calla <andhikamarcellafernanda@gmail.com>
2026-02-02 23:44:53 +0700

    Update server.js

--- worker-service/server.js
+++ worker-service/server.js
@@ -280,7 +280,7 @@ async function processJob(jobId, jobOptions) {
 
     await ytDlp(normalizedUrl, {
       output: downloadTemplate,
-      format: 'bestaudio/bestvideo+bestaudio/best',
+      format: 'bestaudio/best',
       extractAudio: false,
       quiet: true,
       ...(cookiesPath ? { cookies: cookiesPath } : {}),
