6458417c53a00bd26db8f61edd872e165c71905f
cellauu <andhikamarcellafernanda@gmail.com>
2026-07-17 23:18:47 +0700

    Add image, story, reels, and mixed-media CLI modes

--- cli/src/cli-options.js
+++ cli/src/cli-options.js
@@ -22,6 +22,9 @@ export function parseCliOptions(argv = []) {
     cookiesPath: '',
     initialMode: 'video',
     initialPlaylist: false,
+    forceGallery: false,
+    forceVideo: false,
+    galleryInclude: '',
   };
 
   for (let index = 0; index < argv.length; index += 1) {
@@ -55,9 +58,37 @@ export function parseCliOptions(argv = []) {
         break;
       case '--audio':
         options.initialMode = 'audio';
+        options.forceGallery = false;
+        options.forceVideo = false;
         break;
       case '--video':
         options.initialMode = 'video';
+        options.forceVideo = true;
+        options.forceGallery = false;
+        break;
+      case '--image':
+      case '--images':
+      case '--gallery':
+        options.initialMode = 'video';
+        options.forceGallery = true;
+        options.forceVideo = false;
+        break;
+      case '--auto':
+        options.initialMode = 'video';
+        options.forceGallery = false;
+        options.forceVideo = false;
+        break;
+      case '--stories':
+        options.initialMode = 'video';
+        options.forceGallery = true;
+        options.forceVideo = false;
+        options.galleryInclude = 'stories';
+        break;
+      case '--all-media':
+        options.initialMode = 'video';
+        options.forceGallery = true;
+        options.forceVideo = false;
+        options.galleryInclude = 'all';
         break;
       case '--playlist':
         options.initialPlaylist = true;
@@ -87,17 +118,24 @@ export function helpText() {
     + 'Opsi:\n'
     + '  -h, --help            Tampilkan bantuan\n'
     + '  -v, --version         Tampilkan versi\n'
-    + '      --diagnose        Cek Node.js, yt-dlp, FFmpeg, output, dan update\n'
+    + '      --diagnose        Cek Node.js, yt-dlp, gallery-dl, FFmpeg, output, dan update\n'
     + '      --check-update    Cek versi terbaru di npm\n'
     + '      --update          Update otomatis ke ytconv@latest\n'
     + '      --no-update-check Matikan pengecekan update saat aplikasi dibuka\n'
-    + '      --audio           Mulai dalam mode audio\n'
-    + '      --video           Mulai dalam mode video\n'
+    + '      --auto            Deteksi video, audio, gambar, carousel, reel, atau story\n'
+    + '      --audio           Ambil audio dengan yt-dlp/FFmpeg\n'
+    + '      --video           Paksa hanya jalur video yt-dlp\n'
+    + '      --image           Paksa jalur gambar/gallery-dl\n'
+    + '      --stories         Ambil Instagram Stories dari URL profil/story (perlu cookies)\n'
+    + '      --all-media       Ambil post, reels, stories, dan highlights dari profil Instagram\n'
     + '      --playlist        Aktifkan playlist\n'
     + '  -o, --output PATH     Pilih folder hasil\n'
     + '      --cookies FILE    Gunakan cookies.txt Netscape\n\n'
     + 'Contoh:\n'
-    + '  ytconv "https://www.youtube.com/watch?v=..."\n'
+    + '  ytconv "https://www.instagram.com/p/..."\n'
+    + '  ytconv --image "https://www.pinterest.com/pin/..."\n'
+    + '  ytconv --stories --cookies cookies.txt "https://www.instagram.com/username/"\n'
+    + '  ytconv --all-media --cookies cookies.txt "https://www.instagram.com/username/"\n'
     + '  ytconv --audio --output D:\\Music "https://..."\n'
     + '  ytconv --check-update\n'
     + '  ytconv --update\n'
