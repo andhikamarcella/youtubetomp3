fe074868fca984036e97f085e9d9b9d051893d05
cellauu <andhikamarcellafernanda@gmail.com>
2026-07-17 18:01:19 +0700

    Use Android Downloads folder when Termux storage is available

--- cli/bin/ytconv.js
+++ cli/bin/ytconv.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 
+import fs from 'node:fs';
+import os from 'node:os';
+import path from 'node:path';
 import process from 'node:process';
 import { runApp } from '../src/ui.js';
+import { isTermux } from '../src/platform.js';
+
+if (isTermux() && !process.env.YTCONV_OUTPUT) {
+  const androidDownloads = path.join(os.homedir(), 'storage', 'downloads');
+  if (fs.existsSync(androidDownloads)) {
+    process.env.YTCONV_OUTPUT = path.join(androidDownloads, 'YTConv');
+  }
+}
 
 const initialUrl = process.argv.slice(2).find((argument) => /^https?:\/\//iu.test(argument)) ?? '';
 
