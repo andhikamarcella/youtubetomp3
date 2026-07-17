9e232c3346f0c0bde1f27edceb9b6b7df84c5d5a
cellauu <andhikamarcellafernanda@gmail.com>
2026-07-17 22:39:20 +0700

    Add update-related CLI options

--- cli/src/cli-options.js
+++ cli/src/cli-options.js
@@ -14,6 +14,9 @@ export function parseCliOptions(argv = []) {
     help: false,
     version: false,
     diagnose: false,
+    checkUpdate: false,
+    update: false,
+    noUpdateCheck: false,
     initialUrl: '',
     outputDirectory: '',
     cookiesPath: '',
@@ -40,6 +43,15 @@ export function parseCliOptions(argv = []) {
         break;
       case '--diagnose':
         options.diagnose = true;
+        break;
+      case '--check-update':
+        options.checkUpdate = true;
+        break;
+      case '--update':
+        options.update = true;
+        break;
+      case '--no-update-check':
+        options.noUpdateCheck = true;
         break;
       case '--audio':
         options.initialMode = 'audio';
@@ -73,16 +85,21 @@ export function helpText() {
     + '  ytconv [link] [opsi]\n'
     + '  npx -y ytconv@latest [link]\n\n'
     + 'Opsi:\n'
-    + '  -h, --help          Tampilkan bantuan\n'
-    + '  -v, --version       Tampilkan versi\n'
-    + '      --diagnose      Cek Node.js, yt-dlp, FFmpeg, dan folder output\n'
-    + '      --audio         Mulai dalam mode audio\n'
-    + '      --video         Mulai dalam mode video\n'
-    + '      --playlist      Aktifkan playlist\n'
-    + '  -o, --output PATH   Pilih folder hasil\n'
-    + '      --cookies FILE  Gunakan cookies.txt Netscape\n\n'
+    + '  -h, --help            Tampilkan bantuan\n'
+    + '  -v, --version         Tampilkan versi\n'
+    + '      --diagnose        Cek Node.js, yt-dlp, FFmpeg, output, dan update\n'
+    + '      --check-update    Cek versi terbaru di npm\n'
+    + '      --update          Update otomatis ke ytconv@latest\n'
+    + '      --no-update-check Matikan pengecekan update saat aplikasi dibuka\n'
+    + '      --audio           Mulai dalam mode audio\n'
+    + '      --video           Mulai dalam mode video\n'
+    + '      --playlist        Aktifkan playlist\n'
+    + '  -o, --output PATH     Pilih folder hasil\n'
+    + '      --cookies FILE    Gunakan cookies.txt Netscape\n\n'
     + 'Contoh:\n'
     + '  ytconv "https://www.youtube.com/watch?v=..."\n'
     + '  ytconv --audio --output D:\\Music "https://..."\n'
+    + '  ytconv --check-update\n'
+    + '  ytconv --update\n'
     + '  ytconv --diagnose\n';
 }
