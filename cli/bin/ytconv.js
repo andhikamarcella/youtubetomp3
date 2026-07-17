bd9e43c89484c9dffa2ba78ac75631b3a6281bef
cellauu <andhikamarcellafernanda@gmail.com>
2026-07-17 14:05:23 +0700

    Allow YTConv to accept a URL argument

--- cli/bin/ytconv.js
+++ cli/bin/ytconv.js
@@ -3,13 +3,9 @@
 import process from 'node:process';
 import { runApp } from '../src/ui.js';
 
-process.on('SIGINT', () => {
-  process.stdout.write('\n');
-  console.log('Sampai jumpa di YTConv!');
-  process.exit(130);
-});
+const initialUrl = process.argv.slice(2).find((argument) => /^https?:\/\//iu.test(argument)) ?? '';
 
-runApp().catch((error) => {
+runApp({ initialUrl }).catch((error) => {
   process.stdout.write('\n');
   console.error(`YTConv berhenti: ${error instanceof Error ? error.message : String(error)}`);
   process.exitCode = 1;
